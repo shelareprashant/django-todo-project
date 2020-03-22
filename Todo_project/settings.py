10c6f267b1ce819da64af311353c852090f907cf
shelareprashant <shelareprashant15@gmail.com>
2020-03-22 02:24:20 +0000

    added changes from pyany

--- Todo_project/settings.py
+++ Todo_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c0m#^zer^uk5bu_c^rb3+xn0)i%uc10b)*4@5m2#)l!6_zpeda'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kamesh09.pythonanywhere.com']
 
 
 # Application definition
@@ -119,4 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_URL = '/login'
