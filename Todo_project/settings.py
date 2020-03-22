14725c074a4dd747218d9f9ff4c41a440d9e90a3
Prashant Shelare <shelareprashant15@gmail.com>
2020-03-22 08:24:06 +0530

    new local setting!

--- Todo_project/settings.py
+++ Todo_project/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_URL = '/login'
+
+try:
+    from .local_settings import *
+except:
+    print('Look like no local file. You must be in production')
